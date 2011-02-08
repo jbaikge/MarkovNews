c07eba89f75f0931a7c4af580789ddbf217915d9
Jake Tews <jtews@okco.com>
2011-02-08 06:00:36 -0500

    Fixed the SQL errors and added BrainBase::category and BrainBase::origin for adding/fetching category/origin IDs

--- BrainBase.py
+++ BrainBase.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 
 class BrainBase:
 	def __init__(self, filename = './brainbase.db'):
@@ -24,19 +25,31 @@ class BrainBase:
 				UNIQUE(word)
 			)""",
 			"""CREATE TABLE IF NOT EXISTS wordpaths (
-				current_word INTEGER,
-				next_word INTEGER NOT NULL,
+				current INTEGER,
+				next INTEGER NOT NULL,
 				origin_id INTEGER NOT NULL,
-				FOREIGN KEY current_word REFERENCES words(id) ON DELETE CASCADE,
-				FOREIGN KEY next_word REFERENCES words(id) ON DELETE CASCADE,
-				FOREIGN KEY origin_id REFERENCES origins(id) ON DELETE CASCADE
+				FOREIGN KEY (current) REFERENCES words(id) ON DELETE CASCADE,
+				FOREIGN KEY (next) REFERENCES words(id) ON DELETE CASCADE,
+				FOREIGN KEY (origin_id) REFERENCES origins(id) ON DELETE CASCADE
 			)"""
 		]
 		for sql in creates:
 			self.cursor.execute(sql)
 		self.connection.commit()
-		
-		
+	def category(self, category_name):
+		self.cursor.execute("INSERT OR IGNORE INTO categories (name) VALUES (?)", (category_name,))
+		self.connection.commit()
+		self.cursor.execute("SELECT id FROM categories WHERE name = ?", (category_name,))
+		return self.cursor.fetchone()[0]
+	def origin(self, url):
+		self.cursor.execute("INSERT OR IGNORE INTO origins (url, added) VALUES (?, ?)", (url, time.time()))
+		self.connection.commit()
+		self.cursor.execute("SELECT id FROM origins WHERE url = ?", (url,))
+		return self.cursor.fetchone()[0]
 
 bb = BrainBase()
 bb.initialize()
+print "Category 'Tech': %d" % bb.category('Tech')
+print "Category 'IT': %d" % bb.category('IT')
+print "URL 'http://www.google.com': %d" % bb.origin('http://www.google.com')
+print "URL 'http://www.monkeys.com': %d" % bb.origin('http://www.monkey.com')

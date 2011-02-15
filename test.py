2a58bf769933865321b35615a15728db86a265f0
Jake Tews <jtews@okco.com>
2011-02-15 07:22:50 -0500

    Removed commented code, added query for pulling paths in comments.

--- test.py
+++ test.py
@@ -3,15 +3,6 @@ import urllib2
 import BrainBase
 from PageParser import PageParser
 
-#url = 'http://www.theregister.co.uk/2011/02/11/nokia_microsoft_more_details/'
-
-#o = BrainBase.Origin(url)
-#f = urllib2.urlopen(url)
-#p = PageParser()
-#p.set_origin(o)
-#content = f.read()
-#p.feed(content)
-
 urls = [
 	"file:///usr/share/doc/python-doc/html/library/sys.html",
 	"file:///usr/share/doc/python-doc/html/library/warnings.html",
@@ -20,8 +11,6 @@ urls = [
 for url in urls:
 	p = PageParser(url)
 	p.feed(urllib2.urlopen(url).read())
+	p.save()
 
-#db = BrainBase.BrainBase()
-#print db.get_id("category", {"name": "Technology"})
-#print db.get_id("word", {"word": "oink"})
-#print db.get_id("origin", {"url": "http://www.google.com"})
+#SELECT current_word.word, next_word.word, count(*) AS cnt FROM wordpaths LEFT JOIN words AS current_word ON(current = current_word.id) LEFT JOIN words AS next_word ON(next = next_word.id) GROUP BY current, next ORDER BY cnt DESC LIMIT 30;

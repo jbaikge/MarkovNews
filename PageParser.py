e0eb96e801d4018892ee4eaaa63717b27ee4a097
Jake Tews <jtews@okco.com>
2011-02-16 08:31:35 -0500

    Utilizing new BrainBase::exists() method to only parse URLs that have not been previously parsed.

--- PageParser.py
+++ PageParser.py
@@ -1,19 +1,35 @@
 from HTMLParser import HTMLParser
+import urllib2
 
 from BrainBase import BrainBase
 
 class PageParser(HTMLParser):
+	collect_data = True
+	ignore_tags = ['script', 'title', 'style']
+
 	def __init__(self, url):
 		self.brainbase = BrainBase()
-		self.origin_id = self.brainbase.get_id("origin", url)
-		HTMLParser.__init__(self)
+		if self.brainbase.exists("origin", url) == 0:
+			print "parsing",
+			self.origin_id = self.brainbase.get_id("origin", url)
+			HTMLParser.__init__(self)
+
+			contents = urllib2.urlopen(url).read().decode('utf-8')
+			self.feed(contents)
+			self.brainbase.save()
+		print
 
 	def handle_data(self, data):
+		if not self.collect_data:
+			return
 		words = data.split()
 		for word in words:
-			print word,
-			word_id = self.brainbase.get_id("word", word)
-			self.brainbase.add_word_path(self.origin_id, word_id)
+			try:
+				word = word.decode('utf-8')
+				word_id = self.brainbase.get_id("word", word)
+				self.brainbase.add_word_path(self.origin_id, word_id)
+			except UnicodeEncodeError:
+				pass
 
-	def save(self):
-		self.brainbase.save()
+	def handle_starttag(self, tag, attrs):
+		self.collect_data = tag not in self.ignore_tags

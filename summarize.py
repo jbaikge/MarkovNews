5055162add673e054268aa74e426b8a5e9582876
Jake Tews <jtews@okco.com>
2011-07-18 11:46:26 -0400

    Cleaned up the summary generator and made the table aliases more explicit.

--- summarize.py
+++ summarize.py
@@ -9,16 +9,19 @@ if __name__ == "__main__":
 	connection = database_connect()
 
 	print "Determining seed word"
-	result = connection.execute("SELECT id FROM words ORDER BY RANDOM() LIMIT 1")
+	result = connection.execute("SELECT id FROM words WHERE id > 1 ORDER BY RANDOM() LIMIT 1")
 	seed_id = result.fetchone()[0]
 	print "Seed ID:", seed_id
 
-	query = """SELECT b.word2, a.word1, b.word1, w1.word, w2.word
-		FROM wordpaths a
-			LEFT JOIN wordpaths b ON(a.word2 = b.word1 AND a.position + 1 = b.position AND a.origin_id = b.origin_id)
-			LEFT JOIN words w1 ON(a.word1 = w1.id)
-			LEFT JOIN words w2 ON(b.word1 = w2.id)
-		WHERE a.word1 = ?
+	query = """SELECT paths_b.word2, words_b.word
+		FROM wordpaths paths_a
+			LEFT JOIN wordpaths paths_b ON(
+				paths_a.word2 = paths_b.word1
+				AND paths_a.position + 1 = paths_b.position
+				AND paths_a.origin_id = paths_b.origin_id
+			)
+			LEFT JOIN words words_b ON(paths_b.word1 = words_b.id)
+		WHERE paths_a.word1 = ?
 		ORDER BY RANDOM()
 		LIMIT 1"""
 	
@@ -27,21 +30,11 @@ if __name__ == "__main__":
 	for _ in range(1, 100):
 		result = connection.execute(query, (id,))
 		row = result.fetchone()
-
-		# Not sure what causes this yet
-		if row[0] == None:
+		if row == None:
 			break
-		id = row[0]
-
-		kill_loop = False
-		for word in row[3:]:
-			if word == '':
-				kill_loop = True
-				break
-			else:
-				words += [word]
-		
-		if kill_loop:
-			break
+		id, word = row
+		if word == '':
+			break;
+		words.append(word)
 	
 	print ' '.join(words)
